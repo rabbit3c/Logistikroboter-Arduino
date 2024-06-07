86ea504368b45bd7549489bb299ea7d40bd109c4
Matthieu Cottet <matthieu.cottet@outlook.com>
2024-06-07 09:41:19 +0200

    Changed Code for 2WD

--- Logistikroboter-Arduino.ino
+++ Logistikroboter-Arduino.ino
@@ -1,8 +1,6 @@
 #include <AFMotor.h>
 
 AF_DCMotor motor3(3);
-AF_DCMotor motor4(4);
-AF_DCMotor motor2(2);
 AF_DCMotor motor1(1);
 
 int v = 100; //velocity (0 - 255)
@@ -22,36 +20,31 @@ void loop() {
         break;
       case 'f': //forward
         rightForward(v);
-        delay(20);
         leftForward(v);
         break;
       case 's': //stop
         rightStop();
-        delay(20);
         leftStop();
         break;
       case 'r': //right
         leftForward(v);
-        rightBack(200);
+        rightBack(180);
         break;
       case 'l': //left
         rightForward(v);
-        leftBack(200);
+        leftBack(180);
         break;
       case 'b':
         rightBack(v);
-        delay(50);
         leftBack(v);
       case 't': //turn right or turn left
         if (command == "turn_right") {
-            leftForward(255);
-            delay(40);
+            leftForward(230);
             rightBack(150);
         }
         else if (command == "turn_left") {
-            rightForward(255);
-            delay(40);
-            leftBack(140);
+            rightForward(230);
+            leftBack(130);
         }
         break;
       default:
@@ -63,47 +56,29 @@ void loop() {
 void rightForward(int v) {
   motor1.run(BACKWARD);
   motor1.setSpeed(v);
-  delay(40);
-  motor2.run(BACKWARD);
-  motor2.setSpeed(v);
 }
 
 void leftForward(int v) {
   motor3.run(BACKWARD);
   motor3.setSpeed(v);
-  delay(40);
-  motor4.run(BACKWARD);
-  motor4.setSpeed(v);
 }
 
 void rightBack(int v) {
   motor1.run(FORWARD);
   motor1.setSpeed(v);
-  delay(40);
-  motor2.run(FORWARD);
-  motor2.setSpeed(v);
 }
 
 void leftBack(int v) {
   motor3.run(FORWARD);
   motor3.setSpeed(v);
-  delay(40);
-  motor4.run(FORWARD);
-  motor4.setSpeed(v);
 }
 
 void rightStop() {
   motor1.run(RELEASE);
   motor1.setSpeed(0);
-  delay(40);
-  motor2.run(RELEASE);
-  motor2.setSpeed(0);
 }
 
 void leftStop() {
   motor3.run(RELEASE);
   motor3.setSpeed(0);
-  delay(40);
-  motor4.run(RELEASE);
-  motor4.setSpeed(0);
 }
