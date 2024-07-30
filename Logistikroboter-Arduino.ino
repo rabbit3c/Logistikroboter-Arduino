1d6e6aa790d1f6ed03edd4ca77e87d98f61da8eb
Matthieu Cottet <matthieu.cottet@outlook.com>
2024-07-30 17:47:30 +0200

    Code unloading Package

--- Logistikroboter-Arduino.ino
+++ Logistikroboter-Arduino.ino
@@ -1,12 +1,17 @@
 #include <AFMotor.h>
+#include <Servo.h>
 
 AF_DCMotor motor3(3);
 AF_DCMotor motor1(1);
+
+Servo servo;
 
 int v = 100; //velocity (0 - 255)
 
 void setup() {
   Serial.begin(9600);
+  servo.attach(10);
+  servo.write(93);
 }
 
 void loop() {
@@ -28,13 +33,13 @@ void loop() {
         break;
       case 'r': //right
         leftForward(v);
-        rightBack(180);
+        rightBack(210);
         break;
       case 'l': //left
         rightForward(v);
-        leftBack(180);
+        leftBack(210);
         break;
-      case 'b':
+      case 'b': //back
         rightBack(v);
         leftBack(v);
       case 't': //turn right or turn left
@@ -44,8 +49,12 @@ void loop() {
         }
         else if (command == "turn_left") {
             rightForward(230);
-            leftBack(130);
+            leftBack(150);
         }
+        break;
+      case 'u': //unload (using servo) ur or ul for right and left
+        char dir = command[1];
+        unload(dir);
         break;
       default:
         break;
@@ -81,4 +90,25 @@ void rightStop() {
 void leftStop() {
   motor3.run(RELEASE);
   motor3.setSpeed(0);
+}
+
+void unload(char dir) {
+  if (dir == 'r') {
+    for (int i = 0; i < 10; i++) {
+      servo.write(73);
+      delay(100);
+      servo.write(83);
+      delay(100);
+    }
+  }
+  else {
+   for (int i = 0; i < 10; i++) {
+      servo.write(113);
+      delay(100);
+      servo.write(103);
+      delay(100);
+    }
+  }
+  delay(200);
+  servo.write(93);
 }
